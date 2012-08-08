d65d84128b908ab4dab88cd4144f40a0aa15b37c
mjeffery <michael.jeffery@gmail.com>
2012-08-08 01:03:55 -0400

    preview command creates a basic web server for hosting the compiled project.  This server listens for changes in the source or configuration files.  Currently it is not very tolerant of file renaming. For example, it cannot detect when a file is create/renamed to a missing module.  Will need to change the listening methodology (to directories of modules as well) for proper file detection

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var fs = require('fs'),
 	path = require('path'),
 	_ = require('underscore'),
 	build = require('./lib/build'),
-	createProject = require('./lib/project'),
+	project = require('./lib/project'),
+	preview = require('./lib/preview'),
 	Dependencies = require('./lib/Dependencies'),
 	defaults = require('./lib/default.config');
 
@@ -10,7 +11,7 @@ var PACKAGING = ['global', 'amd'];
 
 //read the command line arguments
 var opts = require('optimist')
-	.usage('')
+	.usage('\nUsage: $0 build|preview')
 	.options({
 		'o' : {
 			alias: 'output',
@@ -18,9 +19,9 @@ var opts = require('optimist')
 					  'default: ./uncommon.js'
 		},
 		'p':{
-			alias: 'packaging',
-			describe: '' +
-					  '\ndefault: global'
+			alias: 'port',
+			describe: '',
+			'default': 8888 //TODO maybe use newline at the end of describe to keep consistent with other options
 		},
 		'main': {
 			describe: 'path to the main javascript file.  Only CommonJS modules reachable ' +
@@ -31,6 +32,15 @@ var opts = require('optimist')
 			describe: ''
 		},
 		'no-conflict': {
+			boolean: true,
+			describe: ''
+		},
+		'global': {
+			boolean: true,
+			describe: ''
+		},
+		'amd': {
+			boolean: true,
 			describe: ''
 		}
 	}),
@@ -53,15 +63,23 @@ var cmd 		 = argv._[0] 	  || 'build',
 	output       = argv.output    || config.output    || defaults.output,
 	main         = argv.main 	  || config.main      || defaults.main,
 	name 		 = argv.name	  || config.name	  || defaults.name,
-	dependencies = new Dependencies(config.dependencies || defaults.dependencies);
+	dependencies = new Dependencies(config.dependencies || defaults.dependencies),
+	port 		 = argv.port;
 	
 //process commands
 if(cmd === 'build') {
-	var modules = createProject(main, dependencies);
+	var modules = project.create(main, dependencies);
 	fs.writeFileSync(output, build(name, modules, dependencies));
 }
 else if(cmd === 'preview') {
-	
+	preview.createServer()
+		.on('depend', function(file) { console.log(' + Watching "' + file + '" for changes')})
+		.on('modified', function(file) { console.log(' * The file "' + file + '" has been modified')})
+		.on('undepend', function(file) { console.log(' - Removing "' + file + '" from the watch list')})
+		.on('startBuild', function() { console.log('Building...')})
+		.on('endBuild', function(file) { console.log('Successfully built "' + file + '"')})
+		.on('error', function(err) { console.error(err) })
+		.listen(port);
 }
 else {
 	console.error('Unkown command "' + cmd + '"');
